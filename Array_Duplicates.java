76891e62ea5cd3a846e9f4d5482a4092282fbb8d
SHIVAM-KUMAR-git <shivamkumarccpl@gmail.com>
2026-04-29 23:19:15 +0530

    new

--- Array_Duplicates.java
+++ Array_Duplicates.java
@@ -14,3 +14,40 @@ Input: arr[] = [3, 1, 2]
 Output: []
 Explanation: There is no repeating element in the array, so the output is empty.*/
 
+import java.util.*;
+
+class Solution {
+
+    public ArrayList<Integer> findDuplicates(int[] arr) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            if (arr[i] == arr[i + 1]) {
+                list.add(arr[i]);
+            }
+        }
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Solution obj = new Solution();
+
+        System.out.println(obj.findDuplicates(arr));
+    }
+}
